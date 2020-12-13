cc518e7f2a2fc292d4d429f9c1f096e6c23866af
chenxiangren <747039150@qq.com>
2020-12-13 17:37:21 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,32 @@
+"""
+print ("你好，张三!") #字符串
+print (2333) #整数
+print (2.333) #小数
+print (True) #布尔值
+print (())  #元组
+print ([]) #数组
+print ({}) #字典
+print (3*9)
+print (2>3)
+print (2.33+4445>400)
+name = "张三"
+print (name)
+"""
+'''a = input("输入的内容：")
+print ("你输入的内容是："+a)'''
+# b = float (input("请输入"))
+# c = float (input("请输入"))
+# d = float (input("请输入"))
+# e = float (input("请输入"))
+# i = float (b+c+d+e)
+# print (i)
+a = (input("请输入"))
+b = (input("请输入"))
+i = len(a+b)
+print("你输入了" ,str(i) ,"字符" ) 
+# s1 = str (dasdasda"张三");
+# s2 = str ("李四");
+# s3 = str ("陈五");
+# s = str ("他们都是同学："+s1 + s2 + s3)
+# print (s)
+
