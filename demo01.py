366cf94e3f43764c8ef4a15cf55964930fe457df
chenxiangren <747039150@qq.com>
2020-12-15 21:35:48 +0800

    12月15上传到github

--- demo01.py
+++ demo01.py
@@ -36,7 +36,7 @@ print ("你输入的内容是："+a)'''
 
 
 #元组
-# a = (1,2,3,4,5,"尼玛","尼玛","尼玛","尼玛","尼玛","你妈",True,False)
+# a = (1,2,3,4,5,"尼玛","尼玛","尼玛","尼玛","你妈",True,False)
 # print (a[0:5])
 # print (a[5:11])
 # print (a[11:])
@@ -55,17 +55,58 @@ print ("你输入的内容是："+a)'''
 
 #数组
 a = [1,2,3,4,5,"尼玛","尼玛","尼玛","尼玛","尼玛","你妈",True,False]
-#a.append("帅哥") #往数组中添加数据
-#a.append(input())#往数组中添加数据键盘上输入
-# a.insert(0,"姐姐")
-#append插入的值默认为是最后一个 insert是可以选择插在哪个下标之的格式:insert(下标,插入内容)
-# print (a)
-# b = a.pop(6)#pop剪切的意思
+# #a.append("帅哥") #往数组中添加数据
+# #a.append(input())#往数组中添加数据键盘上输入
+# # a.insert(0,"姐姐")
+# #append插入的值默认为是最后一个 insert是可以选择插在哪个下标之的格式:insert(下标,插入内容)
+# # print (a)
+# # b = a.pop(6)#pop剪切的意思
+# # print(a)
+# # print(b)
+# #a.clear() #clear清空a里面的所有数组
+# #print(a)
+# x = ["你好","不好"]
+# a.extend(x)
 # print(a)
-# print(b)
-#a.clear() #clear清空a里面的所有数组
-#print(a)
-x = ["你好","不好"]
-a.extend(x)
-print(a)
+# a.remove("尼玛")
+# print(a)
 #元组与数组的区别，元组不能添加数据，而数组就可以
+
+
+#字典的特点
+"""
+1、字典中的值没有顺序
+2、字典的结构必须是 键值对的结果 key:value
+3、字典的取值，是通过key去取value
+"""
+# a={"username":"张三","password":"zhangsan","age":"80"}
+# # print(a["username"]) #取username的值
+# # a["身高"] = "183" #增加一个身高
+# # print(a)
+# # a["username"] = "李四" #修改username张三的值改为李四
+# # print(a)
+# b = a.get("username")#取出username里面的值
+# print(b)
+# b = a.pop("username")#剪切出usename里面的值
+# print(b)
+# a.update(username="李四")#添加一个username并取值为李四，update可以修改，也可以新增
+# print(a)
+# a.update(name="陈武")
+# print(a)
+# del a["password"]
+# print(a)
+# del a[1]
+# print(a)
+
+
+# a={}
+# a["name"] = str (input("请输入你的姓名："))
+# a["age"] = int (input("请输入你的身高："))
+# a["sex"] = int (input("请输入你的年龄"))
+# print(str (a))
+# name = a.get("name")
+# age = a.get("age")
+# sex = a.get("sex")
+# print("姓名：" + name)
+# print("身高："+ str (age))
+# print("年龄:"+ str (sex))
